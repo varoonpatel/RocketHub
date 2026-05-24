12431dd1a8e60c331735c4cf0d79af19f58e843f
Varun Patel <ivarunpatel@gmail.com>
2026-05-24 18:58:38 -0400

    RCKH-3: Setup Apollo GraphQL Config and Create Login mutation

---
+++ Modules/Sources/GraphQLAPI/RocketAPI/Package.swift
@@ -0,0 +1,30 @@
+// swift-tools-version:6.1
+
+import PackageDescription
+
+let package = Package(
+  name: "RocketAPI",
+  platforms: [
+    .iOS(.v15),
+    .macOS(.v12),
+    .tvOS(.v15),
+    .watchOS(.v8),
+    .visionOS(.v1),
+  ],
+  products: [
+    .library(name: "RocketAPI", targets: ["RocketAPI"]),
+  ],
+  dependencies: [
+    .package(url: "https://github.com/apollographql/apollo-ios", exact: "2.1.2"),
+  ],
+  targets: [
+    .target(
+      name: "RocketAPI",
+      dependencies: [
+        .product(name: "ApolloAPI", package: "apollo-ios"),
+      ],
+      path: "./Sources"
+    ),
+  ],
+  swiftLanguageModes: [.v6, .v5]
+)
